891288886f13c1b05dc4dc45994288e1b679582d
Oliver Rushworth <oliver@oliverushworth.uk>
2020-03-19 16:46:55 +0000

    add article type

--- .eleventy.js
+++ .eleventy.js
@@ -1,47 +1,61 @@
-const { isFinite } = Number;
-
-const isCategory = post =>
-  Array.isArray(post.data.tags) && post.data.tags.includes("categories");
+const ARTICLE_HEADING_REGEX = /^#+([^#)]+)$/gm;
+const RULER_REGEX = /^(-{3,}|\*{3,}|_{3,})$/;
 
 const sortByPosition = (a, b) => a.data.position - b.data.position || -1;
 
-const convertToObject = (acc, current) => ({
-  ...acc,
-  [current.category]: current
-});
+const isArticle = post => post.data.type === 'article';
+
+const getHeadingData = (article) => (
+  article.template.inputContent
+    .match(ARTICLE_HEADING_REGEX)
+    .map(s => {
+      const heading = s.replace(ARTICLE_HEADING_REGEX, '$1').trim();
+      const [title, ...content] = heading.split('\n');
+
+      const url = article.url.replace(/\/$/, '');
+      const fragment = title.replace(' ', '').toLowerCase();
+      const text = content
+        .filter(t => t.replace(RULER_REGEX, ''))
+        .join(' ');
+
+      return {
+        title,
+        text,
+        fragment,
+        url: url + '#' + fragment,
+      }
+    })
+);
 
 module.exports = function(config) {
-  config.addLayoutAlias("default", "layouts/base.njk");
+  config.addLayoutAlias('default', 'layouts/base.njk');
   // pass some assets right through
-  config.addPassthroughCopy("./src/images");
-  config.addPassthroughCopy("./src/css");
-  config.addPassthroughCopy("./src/js");
+  config.addPassthroughCopy('./src/images');
+  config.addPassthroughCopy('./src/css');
+  config.addPassthroughCopy('./src/js');
 
-  config.addCollection("sortedCategories", collections =>
+  config.addCollection('articles', collections => (
     collections
       .getAll()
-      .filter(isCategory)
+      .filter(isArticle)
       .sort(sortByPosition)
-  );
-
-  config.addCollection("categoryData", collections =>
-    collections
-      .getAll()
-      .filter(isCategory)
-      .map(post => post.data)
-      .reduce(convertToObject, {})
-  );
+      .map(article => {
+        // trying to clone article via ... throws an error
+        article.headings = getHeadingData(article);
+        return article;
+      })
+  ));
 
   return {
     dir: {
-      input: "src/",
-      output: "_site",
-      data: "_data/"
+      input: 'src/',
+      output: '_site',
+      data: '_data/'
     },
-    templateFormats: ["njk", "md", "11ty.js"],
-    htmlTemplateEngine: "njk",
-    markdownTemplateEngine: "njk",
+    templateFormats: ['njk', 'md', '11ty.js'],
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk',
     passthroughFileCopy: true,
-    pathPrefix: "/nhs-help-centre/"
+    pathPrefix: '/nhs-help-centre/'
   };
 };
--- src/test-article.md
+++ src/test-article.md
@@ -1,8 +1,24 @@
 ---
-title: How to change your NHS login password
-tags: manage
-fragment: changepassword
+title: test article
+subtitle: this is a subtitle
+type: article
+position: 1
+layout: layouts/article.njk
 ---
+#    Heading 1
+some text
+some more test
+***
+adsffads
+dasffa
+adsffdfa
+fadfas
+***
+
+### subheading
+
+###        Heading 2
 You can change your password by visiting your NHS login settings. Under the heading ‘your personal information’ select ‘change password’.
 
 You will be asked to enter your current password, and then your new one to confirm the change.
+***
