98e15ca47c912060592e3f103f6f71c04239e3be
Oliver Rushworth <oliver@oliverushworth.uk>
2020-04-20 17:05:18 +0100

    Remove parameterised path prefix

--- .eleventy.js
+++ .eleventy.js
@@ -51,6 +51,6 @@ module.exports = function(config) {
     htmlTemplateEngine: 'njk',
     markdownTemplateEngine: 'njk',
     passthroughFileCopy: true,
-    pathPrefix: process.env.pathPrefix
+    pathPrefix: ''
   };
 };
