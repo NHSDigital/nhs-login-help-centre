e121d435c92fc1066a5711135efb4091e80f490a
Oliver Rushworth <oliver@oliverushworth.uk>
2020-04-29 17:15:24 +0100

    update build

--- src/js/contact-page.js
+++ src/js/contact-page.js
@@ -1,17 +1,5 @@
 (function() {
-    const idToken = getJSONCookie('id_token');
-    const emailWithoutCookieForm = document.querySelector('#email-without-cookie');
-    const emailWithCookieForm = document.querySelector('#email-with-cookie');
-    const emailForm = document.querySelector('#email-form');
     const errorCode = getParam('error');
-
-    if (idToken) {
-        emailForm.removeChild(emailWithoutCookieForm);
-        emailWithCookieForm.style.display = '';
-    } else {
-        emailForm.removeChild(emailWithCookieForm);
-        emailWithoutCookieForm.style.display = '';
-    }
 
     if (errorCode) {
         document.querySelector('#error-code-select').value = errorCode;
