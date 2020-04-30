960f7c0c626cafe57241d96ff323d28047023e3d
Oliver Rushworth <oliver@oliverushworth.uk>
2020-04-30 09:38:11 +0100

    Push contact form changes

--- src/js/contact-page.js
+++ src/js/contact-page.js
@@ -1,7 +1,14 @@
 (function() {
     const errorCode = getParam('error');
+    const form = document.getElementById('contact-us-form');
 
     if (errorCode) {
-        document.querySelector('#error-code-select').value = errorCode;
+        document.querySelector('#errorcode').value = errorCode;
     }
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const data = new FormData(form).entries();
+        console.log(data)
+    });
 })();
