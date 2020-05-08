cc77fc8ed3b3c68e4c37e37a123137665cb01255
Oliver Rushworth <oliverrushworth@olivers-mbp.home>
2020-05-08 10:56:10 +0100

    Add precommit linting / fixing

--- src/js/consent-cookie.js
+++ src/js/consent-cookie.js
@@ -1,18 +1,20 @@
-const COOKIE_NAME = "nhsuk-cookie-consent";
-const COOKIE_VERSION = 3;
-const COOKIE_LIVE_DAYS = 90;
+const COOKIE_NAME = 'nhsuk-cookie-consent'
+const COOKIE_VERSION = 3
+const COOKIE_LIVE_DAYS = 90
 
 function getConsentCookie() {
-  const cookie = getJSONCookie(COOKIE_NAME) || {};
-  return cookie.version < COOKIE_VERSION ? null : cookie;
+  const cookie = getJSONCookie(COOKIE_NAME) || {}
+  return cookie.version < COOKIE_VERSION ? null : cookie
 }
 
 function setConsentCookie(props) {
-  const cookieJSON = JSON.stringify({ ...props, version: COOKIE_VERSION });
+  const cookieJSON = encodeURIComponent(
+    JSON.stringify({ ...props, version: COOKIE_VERSION })
+  )
 
-  const date = new Date();
-  date.setTime(date.getTime() + COOKIE_LIVE_DAYS * 86400000);
-  const expires = date.toUTCString();
+  const date = new Date()
+    date.setTime(date.getTime() + COOKIE_LIVE_DAYS * 86400000)
+    const expires = date.toUTCString()
 
-  document.cookie = `${COOKIE_NAME}=${encodeURIComponent(cookieJSON)}; expires=${expires}; path=/`;
+    document.cookie = `${COOKIE_NAME}=${cookieJSON}; expires=${expires}; path=/`
 }
