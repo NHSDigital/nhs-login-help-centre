e92eba700acc992e5958d4572e481490fb9c969f
Oliver Rushworth <oliverrushworth@olivers-mbp.home>
2020-05-25 13:44:52 +0100

    Refactor Analytics to make better use of private functions

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -1,35 +1,42 @@
 const Analytics = (function() {
+  // add this for local dev
+  // https://assets.adobedtm.com/launch-EN89caca63ff2c4dae923a38d7d05ec849-development.min.js
+  const ADOBE_URL = 'https://assets.adobedtm.com/launch-ENef935e55aadd4530a5725efe3624e9e2.min.js';
+  const HOTJAR_URL = '/js/hotjar.js';
+
   function trackLinkClick(element) {
-    digitalData.link = {
+    window.digitalData.link = {
       linkName: element.innerText,
       linkDestination: element.getAttribute('href'),
     };
     _satellite.track('tracklink');
   }
 
+  function trackPageView() {
+    window.digitalData.page = {
+      pageInfo: {
+        pageName: document.title,
+      },
+    };
+    _satellite.track('page_view');
+  }
+
+  function createHandlers() {
+    for (const link of document.querySelectorAll('a')) {
+      link.addEventListener('click', () => trackLinkClick(link));
+    }
+  }
+
   return {
     start() {
-      const cookiePreferences = Consent.getCookiePreferences();
+      const { hasAcceptedOptionalCookies } = Consent.getCookiePreferences();
 
-      if (cookiePreferences.hasAcceptedOptionalCookies) {
-        Utils.createScript('/js/hotjar.js');
-        // add this for local dev
-        // Utils.createScript('https://assets.adobedtm.com/launch-EN89caca63ff2c4dae923a38d7d05ec849-development.min.js')
-        Utils.createScript(
-          'https://assets.adobedtm.com/launch-ENef935e55aadd4530a5725efe3624e9e2.min.js'
-        ).then(() => {
-          window.digitalData = {
-            page: {
-              pageInfo: {
-                pageName: document.title,
-              },
-            },
-          };
-
-          _satellite.track('page_view');
-          for (const link of document.querySelectorAll('a')) {
-            link.addEventListener('click', () => trackLinkClick(link));
-          }
+      if (hasAcceptedOptionalCookies) {
+        Utils.createScript(HOTJAR_URL);
+        Utils.createScript(ADOBE_URL).then(() => {
+          window.digitalData = {};
+          trackPageView();
+          createHandlers();
         });
       }
     },
