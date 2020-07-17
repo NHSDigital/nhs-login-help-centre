6f76bb1463c595154436f14031e24c8ebda9e10c
Oliver Rushworth <oliverrushworth@Olivers-MacBook-Pro.local>
2020-07-17 11:47:57 +0100

    Add Environment to 11ty

---
+++ src/_data/environment.js
@@ -0,0 +1,4 @@
+module.exports = {
+  EMAIL_API_URL: process.env.EMAIL_API_URL || 'https://api.dev.signin.nhs.uk/nhs-login-support/send-email',
+  ACCESS_FRONTEND_URL: process.env.ACCESS_FRONTEND_URL || 'https://access.dev.signin.nhs.uk'
+};
