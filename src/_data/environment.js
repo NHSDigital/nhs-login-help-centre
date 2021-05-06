a84c396a4b7d8e92d3f35e4b7f02775d961a4eb6
Lydia Shepherd <lydia.shepherd1@nhs.net>
2021-05-06 09:31:29 +0100

    CID-11176 add extra form metadata and change API URL to go to new endpoint for zendesk

--- src/_data/environment.js
+++ src/_data/environment.js
@@ -1,4 +1,4 @@
 module.exports = {
-  EMAIL_API_URL: process.env.EMAIL_API_URL || 'https://api.dev.signin.nhs.uk/nhs-login-support/send-email',
+  EMAIL_API_URL: process.env.EMAIL_API_URL || 'https://api.dev8.signin.nhs.uk/nhs-login-support/raise-ticket',
   ACCESS_FRONTEND_URL: process.env.ACCESS_FRONTEND_URL || 'https://access.dev.signin.nhs.uk'
 };
