ab4f382b88a63e26c4de808afc643b4f3835149e
Egor-BJSS <egor.kharlamov@bjss.com>
2023-04-26 15:47:27 +0100

    Adding nexus scan

---
+++ nexus_iq_scan.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+REPORT_ID=$2
+
+for D in ./wheels "${@:3}" ; do
+  echo "deps : $D"
+done
+
+if [[ $1 == *"codepipeline/dev-"* || $1 == *"codepipeline/ptl-"* || -n $FORCE_NEXUS_SCAN ]]; then
+  echo "Running Nexus IQ scan, initiator == ${1}, FORCE_NEXUS=${FORCE_NEXUS_SCAN}"
+
+  # Copy python wheels into a folder to scan
+  mkdir -p wheels
+  python scripts/collect_wheels.py
+  # any bespoke scan code goes here
+  source ./scripts/nexus_iq_scan_extras.sh
+  cat nexus-wheels.txt | uniq
+
+
+  # Fetch nexus scanner into a temp folder (so we don't scan itself)
+  if [ ! -e /tmp/nexusiq-cli.jar ] ; then
+    curl https://nexus.mgmt.signin.nhs.uk/repository/maven-nhsd/NexusIQ/NexusIQCLI/1.80.0-01/NexusIQCLI-1.80.0-01.jar --output /tmp/nexusiq-cli.jar
+  fi
+
+  java -jar /tmp/nexusiq-cli.jar -t ${STAGE} -i "$REPORT_ID" -s https://nexus-iq.mgmt.signin.nhs.uk -a "${NEXUS_IQ_USERNAME}:${NEXUS_IQ_PASSWORD}" ./wheels "${@:3}"
+
+else
+  echo "Not running Nexus IQ scan as this is not a dev or ptl build."
+fi
