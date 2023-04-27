a73d2836e6a91308287c2552a0614908591a3fd4
Egor-BJSS <egor.kharlamov@bjss.com>
2023-04-27 10:52:38 +0100

    Scan this repostory

--- scripts/nexus_iq_scan.sh
+++ scripts/nexus_iq_scan.sh
@@ -2,28 +2,15 @@
 
 REPORT_ID=$2
 
-for D in ./wheels "${@:3}" ; do
-  echo "deps : $D"
-done
-
-echo $1
 if [[ $1 == *"codepipeline/helpcentre-live-develop"* || $1 == *"codepipeline/helpcentre-live-master-"* || -n $FORCE_NEXUS_SCAN ]]; then
   echo "Running Nexus IQ scan, initiator == ${1}, FORCE_NEXUS=${FORCE_NEXUS_SCAN}"
-
-  # Copy python wheels into a folder to scan
-  mkdir -p wheels
-  python scripts/collect_wheels.py
-  # any bespoke scan code goes here
-  source ./scripts/nexus_iq_scan_extras.sh
-  cat nexus-wheels.txt | uniq
-
 
   # Fetch nexus scanner into a temp folder (so we don't scan itself)
   if [ ! -e /tmp/nexusiq-cli.jar ] ; then
     curl https://nexus.mgmt.signin.nhs.uk/repository/maven-nhsd/NexusIQ/NexusIQCLI/1.80.0-01/NexusIQCLI-1.80.0-01.jar --output /tmp/nexusiq-cli.jar
   fi
 
-  java -jar /tmp/nexusiq-cli.jar -i "$REPORT_ID" -s https://nexus-iq.mgmt.signin.nhs.uk -a "pipeline:${NEXUS_IQ_PASSWORD}" ./wheels "${@:3}"
+  java -jar /tmp/nexusiq-cli.jar -i "$REPORT_ID" -s https://nexus-iq.mgmt.signin.nhs.uk -a "pipeline:${NEXUS_IQ_PASSWORD}" .
 
 else
   echo "Not running Nexus IQ scan as this is not a dev or ptl build."
