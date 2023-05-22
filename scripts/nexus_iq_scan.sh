d928c4317f1211f9115fab619e0df1c53e8c3a51
Greg Eke <greg.eke2@nhs.net>
2023-05-22 12:22:16 +0100

    Adjust nexus script for help centre

--- scripts/nexus_iq_scan.sh
+++ scripts/nexus_iq_scan.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-if [[ $1 == *"codepipeline/dev-"* || $1 == *"codepipeline/ptl-"* ]]; then
-    echo "Running Nexus IQ scan as this is a dev or ptl build."
+REPORT_ID=$2
+
+if [[ $1 == *"codepipeline/helpcentre-live-develop"* || $1 == *"codepipeline/helpcentre-live-master-"* || -n $FORCE_NEXUS_SCAN ]]; then
+    echo "Running Nexus IQ scan, initiator == ${1}, FORCE_NEXUS=${FORCE_NEXUS_SCAN}"
     curl -L  "https://download.sonatype.com/clm/scanner/nexus-iq-cli-1.151.0-01+565-unix.zip" --output nexusiq-cli.zip
     unzip nexusiq-cli.zip
-    ./nexus-iq-cli -i acc-fe -s https://nexus-iq.mgmt.signin.nhs.uk -a pipeline:$NEXUS_IQ_PASSWORD .
+    ./nexus-iq-cli -i "$REPORT_ID" -s https://nexus-iq.mgmt.signin.nhs.uk -a pipeline:$NEXUS_IQ_PASSWORD .
 else
     echo "Not running Nexus IQ scan as this is not a dev or ptl build."
 fi
