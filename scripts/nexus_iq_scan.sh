acb4cd6ec59a0bcdee134c8724ae279f46d29227
Greg Eke <greg.eke2@nhs.net>
2023-05-22 12:00:39 +0100

    Update nexus script

--- scripts/nexus_iq_scan.sh
+++ scripts/nexus_iq_scan.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-REPORT_ID=$2
-
-if [[ $1 == *"codepipeline/helpcentre-live-develop"* || $1 == *"codepipeline/helpcentre-live-master-"* || -n $FORCE_NEXUS_SCAN ]]; then
-  echo "Running Nexus IQ scan, initiator == ${1}, FORCE_NEXUS=${FORCE_NEXUS_SCAN}"
-
-  # Fetch nexus scanner into a temp folder (so we don't scan itself)
-  if [ ! -e /tmp/nexusiq-cli.jar ] ; then
-    curl https://nexus.mgmt.signin.nhs.uk/repository/maven-nhsd/NexusIQ/NexusIQCLI/1.80.0-01/NexusIQCLI-1.80.0-01.jar --output /tmp/nexusiq-cli.jar
-  fi
-
-  java -jar /tmp/nexusiq-cli.jar -i "$REPORT_ID" -s https://nexus-iq.mgmt.signin.nhs.uk -a "pipeline:${NEXUS_IQ_PASSWORD}" .
-
+if [[ $1 == *"codepipeline/dev-"* || $1 == *"codepipeline/ptl-"* ]]; then
+    echo "Running Nexus IQ scan as this is a dev or ptl build."
+    curl -L  "https://download.sonatype.com/clm/scanner/nexus-iq-cli-1.151.0-01+565-unix.zip" --output nexusiq-cli.zip
+    unzip nexusiq-cli.zip
+    ./nexus-iq-cli -i acc-fe -s https://nexus-iq.mgmt.signin.nhs.uk -a pipeline:$NEXUS_IQ_PASSWORD .
 else
-  echo "Not running Nexus IQ scan as this is not a dev or ptl build."
+    echo "Not running Nexus IQ scan as this is not a dev or ptl build."
 fi
