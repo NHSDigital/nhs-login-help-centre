b3690055e277660d282262f6a44956b9c530545f
Greg Eke <greg.eke2@nhs.net>
2023-05-22 12:42:38 +0100

    Download nexus to tmp folder

--- scripts/nexus_iq_scan.sh
+++ scripts/nexus_iq_scan.sh
@@ -4,9 +4,14 @@ REPORT_ID=$2
 
 if [[ $1 == *"codepipeline/helpcentre-live-develop"* || $1 == *"codepipeline/helpcentre-live-master-"* || -n $FORCE_NEXUS_SCAN ]]; then
     echo "Running Nexus IQ scan, initiator == ${1}, FORCE_NEXUS=${FORCE_NEXUS_SCAN}"
-    curl -L  "https://download.sonatype.com/clm/scanner/nexus-iq-cli-1.151.0-01+565-unix.zip" --output nexusiq-cli.zip
-    unzip nexusiq-cli.zip
-    ./nexus-iq-cli -i "$REPORT_ID" -s https://nexus-iq.mgmt.signin.nhs.uk -a pipeline:$NEXUS_IQ_PASSWORD .
+
+    # Fetch nexus scanner into a temp folder (so we don't scan itself)
+    if [ ! -e /tmp/nexusiq-cli.zip ] ; then
+        curl -L  "https://download.sonatype.com/clm/scanner/nexus-iq-cli-1.151.0-01+565-unix.zip" --output /tmp/nexusiq-cli.zip
+    fi
+
+    unzip /tmp/nexusiq-cli.zip -d tmp
+    ./tmp/nexus-iq-cli -i "$REPORT_ID" -s https://nexus-iq.mgmt.signin.nhs.uk -a pipeline:$NEXUS_IQ_PASSWORD .
 else
     echo "Not running Nexus IQ scan as this is not a dev or ptl build."
 fi
